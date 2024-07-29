11695e33874abe0ddfcdd5a52c7eaab72500d856
midhyun <kfree912@gmail.com>
2024-07-29 19:16:37 +0900

    First Commit

---
+++ src/main/java/slo/slo_spring_server/SloSpringServerApplication.java
@@ -0,0 +1,13 @@
+package slo.slo_spring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SloSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SloSpringServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/slo/slo_spring_server/SloSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package slo.slo_spring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SloSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
