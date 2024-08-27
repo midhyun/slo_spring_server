2b4bb22ab021446ab5a3433ed0c26d4e87943665
midhyun <kfree912@gmail.com>
2024-08-28 00:01:29 +0900

    [feat] Diet Dto, Diet Repository impl

--- src/main/java/slo/slo_spring_server/dto/DietDTO.java
+++ src/main/java/slo/slo_spring_server/dto/DietDTO.java
@@ -1,6 +1,30 @@
 package slo.slo_spring_server.dto;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+import slo.slo_spring_server.domain.diet.DietType;
+import slo.slo_spring_server.exception.ValidEnum;
+
+import java.time.LocalDateTime;
+
+
+@Valid
+@Data
+@Builder
+@AllArgsConstructor
 public class DietDTO {
-    private int id;
+
+    private Long id;
+
+    @NotBlank
+    private Long userId;
+
+    @NotBlank
+    private LocalDateTime dateTime;
+
+    @NotBlank
+    @ValidEnum(enumClass = DietType.class)
+    private DietType dietType;
 
 }
--- src/main/java/slo/slo_spring_server/repository/diet/DietRepository.java
+++ src/main/java/slo/slo_spring_server/repository/diet/DietRepository.java
@@ -1,8 +1,16 @@
 package slo.slo_spring_server.repository.diet;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import slo.slo_spring_server.domain.diet.Diet;
 
+import java.time.LocalDateTime;
+
+@Repository
 public interface DietRepository extends JpaRepository<Diet, Long> {
+    Diet findOneByDietDate(LocalDateTime localDAteTime);
+
+    Diet updateDiet(Diet diet);
+    Diet createDiet(Diet diet);
 
 }
